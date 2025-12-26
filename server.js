640718b5949487959df74c703a52bba7c4fe2bbd
Sreethar170 <sreethar170@gmail.com>
2025-12-26 17:33:55 +0530

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors({
-  origin: process.env.FRONTEND_ORIGIN || "*", 
+  origin: process.env.FRONTEND_ORIGIN || "*", // set this on Render to your Vercel URL
 }));
 app.use(express.json());
 
