47648d234e39b4893ab5c4e5342cd0d97fe8d77b
Sreethar170 <sreethar170@gmail.com>
2025-12-28 01:02:08 +0530

    update

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ const Contact = mongoose.model("Contact", contactSchema);
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  email: { type: String, required: true },
   isAdmin: { type: Boolean, default: false },
   folderAccess: { type: String, enum: ["public", "private"], default: "public" },
   sole: { type: Boolean, default: false },
@@ -158,10 +159,12 @@ app.post("/api/admin/create-user", async (req, res) => {
     const admin = await User.findOne({ username: adminUsername, isAdmin: true });
     if (!admin || admin.password !== adminPassword) return res.status(401).json({ success: false, message: "Unauthorized" });
 
-    const existing = await User.findOne({ username });
-    if (existing) return res.status(400).json({ success: false, message: "Username exists" });
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) return res.status(400).json({ success: false, message: "Username exists" });
+       const existingEmail = await User.findOne({ email });
+    if (existingEmail) return res.status(400).json({ success: false, message: "Email exists" });
 
-    const newUser = new User({ username, password, isAdmin: role === "admin", folderAccess: access || "public" });
+    const newUser = new User({ username, password,email, isAdmin: role === "admin", folderAccess: access || "public" });
     await newUser.save();
 
    try {
